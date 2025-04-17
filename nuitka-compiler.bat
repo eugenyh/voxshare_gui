fd023c26a11a7ee3a56308592d4879b6cada357f
EugenyH <eugenyh@gmail.com>
2025-04-17 12:33:21 +0500

    Append nuitca-compiler.bat

---
+++ nuitka-compiler.bat
@@ -0,0 +1,42 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Check for argument
+if "%~1"=="" (
+    echo Error: Define name fot Python-file for compilation.
+    echo Example: nuitka-compiler.bat myscript.py
+    pause
+    exit /b 1
+)
+
+:: Source file (first argument)
+set "PYTHON_SCRIPT=%~1"
+
+:: Checking for file exists
+if not exist "%PYTHON_SCRIPT%" (
+    echo Error: File "%PYTHON_SCRIPT%" not found!
+    pause
+    exit /b 1
+)
+
+:: Compilation with Nuitka
+echo Compile %PYTHON_SCRIPT% with Nuitka...
+python -m nuitka ^
+   --onefile ^
+   --standalone ^
+   --windows-disable-console ^
+   --windows-icon-from-ico=Icons/logo.ico ^
+   --lto=yes ^
+   --msvc=latest ^
+   --enable-plugin=tk-inter,upx ^
+   --nofollow-import-to=PIL.ImageQt ^
+   "%PYTHON_SCRIPT%"
+
+:: Checking for result
+if errorlevel 1 (
+    echo Compile error! Check settings.
+) else (
+    echo Success! Exectutable file is nutica-dist\
+)
+
+pause
